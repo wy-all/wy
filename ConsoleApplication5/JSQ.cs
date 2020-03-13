fcd1ff67baebc513c08d356687717274b52fe07c
w y <2017396647@qq.com>
2020-03-13 21:18:42 +0800

    添加项目文件。

---
+++ ConsoleApplication5/JSQ.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class JSQ
+    {
+        string num1;
+        string num2;
+        char fh;
+        public string Num1
+        {
+            get
+            {
+                return num1;
+
+            }
+            set
+            {
+                num1 = value;
+            }
+        }
+        public string Num2
+        {
+            get
+            {
+                return num2;
+
+            }
+            set
+            {
+                num2 = value;
+            }
+        }
+        public char Fh
+        {
+            get
+            {
+                return fh;
+
+            }
+            set
+            {
+                fh = value;
+            }
+        }
+        public void Operators(float a, float b)
+        {
+            if (fh == '/')
+                Console.WriteLine(a + "/" + b + "=" + (a / b));
+            if (fh == '+')
+                Console.WriteLine(a + "+" + b + "=" + (a + b));
+            if (fh == '-')
+                Console.WriteLine(a + "-" + b + "=" + (a - b));
+            if (fh == '*')
+                Console.WriteLine(a + "*" + b + "=" + (a * b));
+        }
+        public void Operators()
+        {
+            string c = num1;
+            if (fh == '+')
+                Console.WriteLine(num1 + "连接" + num2 + "=" + string.Concat(num1, num2));
+            if (fh == '-')
+            {
+                char[] b = num1.ToCharArray();
+                char[] a = num2.ToCharArray();
+                for (int i = 0; i < b.Length; i++)
+                {
+                    for (int j = 0; j < a.Length; j++)
+                        if (b[i] == a[j])
+                        {
+                           num1= num1.Replace(a[j], ' ');
+                           
+                        }
+
+                }
+                Console.WriteLine(c + "除去" + num2 + "=" + num1);
+            }
+           
+
+        }
+        public void Equals(JSQ num)
+        {
+            if (num.num1 == num.num2)
+                Console.WriteLine("两数相等");
+        }
+    }
+}
---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JSQ jsq = new JSQ();
+            float  a=0, b=0;
+            Console.WriteLine("请输入表达式，输入一个字符按一下回车！！！");
+            while (true)
+            {
+                jsq.Num1 = Console.ReadLine();
+                string input3 = Console.ReadLine();
+                jsq.Num2 = Console.ReadLine();
+                
+
+                try
+                {
+                    a = float.Parse(jsq.Num1);
+                    b = float.Parse(jsq.Num2);
+                    jsq.Fh = char.Parse(input3);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                jsq.Equals(jsq);
+                jsq.Operators();
+                jsq.Operators(a,b);
+
+                Console.WriteLine("请继续！！");
+            }
+            //Chuan chuan = new Chuan();
+            //Console.WriteLine("请输入字符串");
+            //chuan.C = Console.ReadLine();
+            //chuan.Change(chuan);
+        }
+    }
+}
